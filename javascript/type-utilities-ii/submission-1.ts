ad6e74eaa572e008037cf6ba09b80c42fef95910
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-05-28 21:21:46 -0400

    Add: type-utilities-ii - Submission - 1

---
+++ javascript/type-utilities-ii/submission-1.ts
@@ -0,0 +1,27 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+export function isArray(value: unknown): boolean {
+  throw 'Not implemented!';
+}
+export function isFunction(value: unknown): boolean {
+  throw 'Not implemented!';
+}
+export function isObject(value: unknown): boolean {
+  throw 'Not implemented!';
+}
+export function isPlainObject(value: unknown): boolean {
+  throw 'Not implemented!';
+}
